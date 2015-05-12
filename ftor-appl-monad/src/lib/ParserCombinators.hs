c9f069187dcb5d28e9a5aa5c274554262782a66f
Mate J Kovacs <mkovaxx@gmail.com>
2015-05-12 15:39:15 -0700

    [ftor-appl-monad] add tests and solutions

--- ftor-appl-monad/src/lib/ParserCombinators.hs
+++ ftor-appl-monad/src/lib/ParserCombinators.hs
@@ -8,11 +8,14 @@ import Data.Functor
 
 data Parser t = Parser { runP :: String -> [(t, String)] }
 
+result :: Parser t -> String -> [t]
+result p s = [x | (x, "") <- runP p $ s]
+
 instance Functor Parser where
   fmap f p = Parser $ \s -> [(f x, r) | (x, r) <- runP p s]
 
 instance Applicative Parser where
-  pure x = Parser $ \s -> [(x, "")]
+  pure x = Parser $ \s -> [(x, s)]
   p <*> q = Parser $ \s -> [(f x, r) | (f, t) <- runP p s, (x, r) <- runP q t]
 
 instance Alternative Parser where
@@ -21,3 +24,6 @@ instance Alternative Parser where
 
 instance Monad Parser where
   p >>= f = Parser $ \s -> [(y, r) | (x, t) <- runP p s, (y, r) <- runP (f x) t]
+
+satisfy :: (t -> Bool) -> Parser t -> Parser t
+satisfy f p = Parser $ filter (f . fst) . runP p
--- ftor-appl-monad/src/test/Test.hs
+++ ftor-appl-monad/src/test/Test.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 import Data.Char (isDigit)
+import Data.Functor
+import Control.Applicative
 import Control.Monad
 import System.Exit (exitFailure)
 import Test.QuickCheck
@@ -15,5 +17,50 @@ main = do
 
 tests :: [Property]
 tests =
-  [
+  [ counterexample "parse integer" $
+      \(num :: NonNegative Int) ->
+        let
+          NonNegative int = num
+          input = show int
+        in result parseNum input == [int]
+  , let list = ["one", "two", "three"]
+    in counterexample ("parse length-prefixed list " ++ show list) $
+      let input = join $ map lengthPrefix list
+      in result (many parseLP) input == [list]
+{-
+  , counterexample "parse list of length-prefixed strings" $
+      \(list :: [String]) ->
+        let input = join $ map lengthPrefix list
+        in result (many parseLP) input == [list]
+-}
   ]
+
+lengthPrefix :: String -> String
+lengthPrefix s = show (length s) ++ ":" ++ s
+
+parseLP :: Parser String
+parseLP = do
+  len <- parseNum
+  _ <- char ':'
+  str <- givenLength len
+  return str
+
+parseNum :: Parser Int
+parseNum = fmap read $ many digit
+
+givenLength :: Int -> Parser String
+givenLength len = satisfy ((len==) . length) $ anyString
+
+anyString :: Parser String
+anyString = many anyChar
+
+char :: Char -> Parser Char
+char c = satisfy (c==) anyChar
+
+digit :: Parser Char
+digit = satisfy isDigit anyChar
+
+anyChar :: Parser Char
+anyChar = Parser $ \case
+  (x:xs) -> [(x, xs)]
+  [] -> empty
